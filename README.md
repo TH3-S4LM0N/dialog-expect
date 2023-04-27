648d9d5004544217a7edb83b41c861abeb7123ee
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-04-27 10:57:17 +0000

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dialog-expect
-A crate providing a method to pop up a dialog box on `.expect` or `.unwrap`
+# `dialog-expect`
+A crate providing a method to pop up a dialog box on `.expect` or `.unwrap`. See [docs.rs](https://docs.rs/dialog-expect) for documentation. Supports macOS, Windows, and Linux via Qt5 & GTK.
+
+### Buidling
+Use the cargo `do` alias to build documentation.
---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+//! This crate provides a trait [`DialogExpect`], with a function that unwraps [`Option<T>`] and [`Result<T, E>`] or shows a dialog box and panics.
+
+/// A trait with 1 function, `dialog_expect`, which is implemented for [`std::option::Option<T>`] and [`std::result::Result<T, E>`].
+pub trait DialogExpect<T> {
+    /// Takes `self` and returns the contained value, or shows an error message in a dialog box.
+    /// #### Errors
+    /// This function can fail (the program will panic) if there is no way to spawn a dialog box. It just made no sense for it to return a [`Result<T, E>`].
+    fn dialog_expect<S: AsRef<str>>(self, title: S, msg: S) -> T;
+}
+
+impl<T> DialogExpect<T> for std::option::Option<T> {
+    fn dialog_expect<S: AsRef<str>>(self, title: S, msg: S) -> T {
+        match self {
+            Some(v) => v,
+            None => {
+                let msg = msg.as_ref();
+
+                native_dialog::MessageDialog::new()
+                    .set_title(title.as_ref())
+                    .set_text(msg)
+                    .show_alert()
+                    .unwrap();
+                panic!("{}", msg);
+            }
+        }
+    }
+}
+
+impl<T, E> DialogExpect<T> for std::result::Result<T, E> {
+    fn dialog_expect<S: AsRef<str>>(self, title: S, msg: S) -> T {
+        match self {
+            Ok(v) => v,
+            Err(_) => {
+                let msg = msg.as_ref();
+
+                native_dialog::MessageDialog::new()
+                    .set_title(title.as_ref())
+                    .set_text(msg)
+                    .show_alert()
+                    .unwrap();
+                panic!("{}", msg);
+            }
+        }
+    }
+}
