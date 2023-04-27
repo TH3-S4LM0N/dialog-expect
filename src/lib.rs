d2176817b73a5424950fa57f0e45f0b9bf662258
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-04-27 14:14:28 +0000

    use error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! This crate provides a trait [`DialogExpect`], with a function that unwraps [`Option<T>`] and [`Result<T, E>`] or shows a dialog box and panics.
 
+use native_dialog::MessageType;
+
 /// A trait with 1 function, `dialog_expect`, which is implemented for [`std::option::Option<T>`] and [`std::result::Result<T, E>`].
 pub trait DialogExpect<T> {
     /// Takes `self` and returns the contained value, or shows an error message in a dialog box.
@@ -18,6 +20,7 @@ impl<T> DialogExpect<T> for std::option::Option<T> {
                 native_dialog::MessageDialog::new()
                     .set_title(title.as_ref())
                     .set_text(msg)
+                    .set_type(MessageType::Error)
                     .show_alert()
                     .unwrap();
                 panic!("{}", msg);
@@ -36,6 +39,7 @@ impl<T, E> DialogExpect<T> for std::result::Result<T, E> {
                 native_dialog::MessageDialog::new()
                     .set_title(title.as_ref())
                     .set_text(msg)
+                    .set_type(MessageType::Error)
                     .show_alert()
                     .unwrap();
                 panic!("{}", msg);
